a04dd8b7881c64be3a217e6c2243b6dde7b984b3
qvakie-r2bs <qvakieofficial@gmail.com>
2022-03-02 17:44:42 +0300

    Update themainfile.py

--- themainfile.py
+++ themainfile.py
@@ -5,11 +5,32 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
 
+def action():
+    print('ДОВОЛЕН?')
+
+
 def application():
+    # создали саму приложуху и главное окно
     app = QApplication(sys.argv)
     window = QMainWindow()
 
+    # название \ местоположение + размер окна
+    window.setWindowTitle('toadbats')
     window.setGeometry(800, 400, 500, 500)
+
+    button = QtWidgets.QPushButton(window)
+    button.move(175, 200)
+    button.setFixedHeight(100)
+    button.setFixedWidth(150)
+    button.setText('НАЖМИ ГАНДОН')
+    button.clicked.connect(action)
+
+    are = QtWidgets.QCheckBox(window)  
+
+    main_text = QtWidgets.QLabel(window)
+    main_text.setText('Просто тестирую надписи')
+    main_text.move(250, 0)
+    main_text.adjustSize()
 
     window.show()
     sys.exit(app.exec_())
